0f66dc72c26bc441c2a53049e938d13738357f5a
BhumiPatel2701 <bhumi3243@gmail.com>
2025-09-02 10:40:33 +0000

    summary

---
+++ javascript/pr6.js
@@ -0,0 +1,32 @@
+//primitive
+
+/* 7 types:string,number,boolean,null,undefined,symbol,bigtnt*/
+/*const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+const id =symbol('112')
+const anotherId=symbol('123')
+
+console.log(id===anotherId)
+const bigNumber=1233556678899n*/
+
+
+//refrence(non primitive)
+
+//array,objects,funcations
+
+const heros=["ram","syam","vijay"];
+let myObj = {
+    name:"bhumu",
+    age: 19,
+}
+
+//const myFunction = funcation(){
+
+  
+
+
+//console.log(typeof bigNumber);
