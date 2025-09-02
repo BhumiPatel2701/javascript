fd28c26051dd58bbd78a28cf86efd9d5bbc866d1
BhumiPatel2701 <bhumi3243@gmail.com>
2025-09-02 10:01:32 +0000

    conversion of data type

---
+++ javascript/pr1.js
@@ -0,0 +1,14 @@
+const accountId =12345
+let accountEmail ="bhumi@2701.com"
+var accontpassword="1324"
+accountcity="salvav"
+let accountstate;
+// kai pan value nai aapiye to tenu output undefine aave chhe//
+
+//const accountid=5// not a allowed
+accountEmail="bhumi@123.com"
+accontpassword="1234"
+accountcity="vapi"
+/* prefer not to use a var because of issue in block scope and funcational scope*/
+
+console.log([accountEmail,accontpassword,accountcity]);
---
+++ javascript/pr3.js
@@ -0,0 +1,26 @@
+let score="33abc"
+//"33abc"= string,string,number,nan
+//null= object,object ,number,0
+//undefined=undefined,undefined,number,nan;
+//true=boolean,boolean,number,1
+//33=33
+//33abc=nan
+//true/false=1/0
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//1=true 0=false ""empty to false "bhumi" true
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringNumber);
+
