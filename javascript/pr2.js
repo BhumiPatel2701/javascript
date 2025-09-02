fbf61119e4d8d7fa3f00b418b965c1a4e2bea5ee
BhumiPatel2701 <bhumi3243@gmail.com>
2025-09-02 09:39:42 +0000

    datatypes

---
+++ javascript/pr2.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as newer version
+//alert(2+2)//we are using nodejs,not browser
+
+console.log("bhumi")
+
+let name="bhumi"
+let age=19
+let isLoggedIn =false
+
+//number=>2 to power 53
+//bigint
+//string =>""
+//Boolean =>true/false
+//null=>standalone value
+//undefined=>
+    //symbol=>unique
+
+//object
+console.log(typeof null);
